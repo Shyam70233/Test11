2b0ac579025e69c35db1ed77862ec4483c2bfa58
Shyam70233 <deluxshop468@gmail.com>
2022-06-21 13:15:32 +0530

    DateFirst

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
+  // console.log(new Date().toJSON().slice(0,10));
+  let d = new Date();
+  d = d.toJSON();
+  d = d.slice(0, 10);
+  d = d.split('-');
+  let a = [+d[0], +d[1], +d[2]];
+  console.log(a);
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
     </div>
   );
 }
