55accb56719f560868eb125f3a3a71a698a2851d
Shyam70233 <deluxshop468@gmail.com>
2022-06-21 13:24:13 +0530

    Active Class on Click

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 export default function App() {
   // console.log(new Date().toJSON().slice(0,10));
-  let d = new Date();
-  d = d.toJSON();
-  d = d.slice(0, 10);
-  d = d.split('-');
-  let a = [+d[0], +d[1], +d[2]];
-  console.log(a);
+  const [z, setz] = useState();
+  let a = [
+    { name: 'HTML', lable: 'html, h1, h2, h4' },
+    { name: 'CSS', lable: 'CSSe, h1, h2, h4' },
+    { name: 'Javascript', lable: 'javacsript, h1, h2, h4' },
+  ];
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
+      {a.map((x) => (
+        <h1 onClick={() => setz(x.name)}>{x.name}</h1>
+      ))}
+      {a.map((x) => (
+        <h1 className={x.name === z ? 'active' : ''}>{x.lable}</h1>
+      ))}
     </div>
   );
 }
